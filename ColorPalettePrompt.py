79bd14e26f0130190aa6ba1daed3306ba8d17539
Cristian Fernando Dávila López <krif07@gmail.com>
2024-07-12 19:30:04 -0500

    the get palette promp function was improved

--- ColorPalettePrompt.py
+++ ColorPalettePrompt.py
@@ -3,30 +3,16 @@ class ColorPalettePrompt:
         self.client = client
 
     def get_palette(self, text):
-        prompt = f"""
-        Eres un asistente generador de paletas de colores que responde a indicaciones de texto para generar paletas de colores.
-
-        Debes generar paletas de colores que se ajusten al tema, ambiente o instrucciones en la indicación.
-        Las paletas deben tener entre 2 y 8 colores.
-
-        Q: Convierta la siguiente descripción verbal de una paleta de colores en una lista de colores: El Mar Mediterráneo
-        A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
-
-        Formato deseado: un arreglo JSON de código de colores hexadecimales 
-
-        Q: Convierta la siguiente descripción verbal de una paleta de colores en una lista de colores: {text}
-        A:
-        """
+        messages = [
+            {"role": "system", "content": "Eres un asistente generador de paletas de colores que responde a indicaciones de texto para generar paletas de colores. Debes generar paletas de colores que se ajusten al tema, ambiente o instrucciones en la indicación. Las paletas deben tener entre 2 y 8 colores."},
+            {"role": "user", "content": "Convierta la siguiente descripción verbal de una paleta de colores en una lista de colores: El Mar Mediterráneo"},
+            {"role": "assistant", "content": '["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]'},
+            {"role": "user", "content": f"Convierta la siguiente descripción verbal de una paleta de colores en una lista de colores: {text}"}
+        ]
 
         stream = self.client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-
-                }
-            ],
+            messages=messages,
             max_tokens=200
         )
         return stream.choices[0].message.content
